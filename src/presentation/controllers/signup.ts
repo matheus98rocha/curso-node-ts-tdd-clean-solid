2ee954b9e3c0cddcd83a78e50bd3bf48a29ce85b
matheus98rocha <matheus98rocha@gmail.com>
2023-08-26 13:45:30 -0300

    feat: refactoring on SignUpController

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,16 +1,13 @@
 export class SignUpController {
   handle(httpRequest: any): any {
-    if(!httpRequest.body.name){
-      return {
-        statusCode: 400,
-        body: new Error("Missing param: name"),
-      };
-    }
-    if(!httpRequest.body.email){
-      return {
-        statusCode: 400,
-        body: new Error("Missing param: email"),
-      };
+    const requiredFileds = ["name", "email"];
+    for (const field of requiredFileds) {
+      if (!httpRequest.body[field]) {
+        return {
+          statusCode: 400,
+          body: new Error(`Missing param: ${field}`),
+        };
+      }
     }
   }
 }
